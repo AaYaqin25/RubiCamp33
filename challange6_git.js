2e96cc73b6fb2c8434216a116305410c3c775639
Ahmad Ainul Yaqin <bloodsakin@gmail.com>
2022-10-13 21:28:42 +0700

    Revisi challange 6

--- challange6_git.js
+++ challange6_git.js
@@ -1,12 +1,19 @@
 function sentencesManipulation(sentence) {
-    let str = sentence.replace("pergi", "ergipnyo");
-    let strKata = str.replace("ke", "eknyo");
-    let strKata2 = strKata.replace("pasar", "asarpnyo");
-    let strKata3 = strKata2.replace("bersama", "ersamabnyo");
+    let str = arguments[0].split(" ");
+    let result = [];
 
-
-    console.log(strKata3);
-
+    let i = 0;
+    while (i < str.length) {
+        let j = 0;
+        if (str[i][j] === "a" || str[i][j] === "i" || str[i][j] === "u" || str[i][j] === "e" || str[i][j] === "o") {
+            result.push(str[i]);
+        } else {
+            result.push(str[i].substring(1) + str[i][j] + "nyo")
+        }
+        i++;
+    }
+    console.log(result.toString().replace(/,/g, " "));
 }
 
 sentencesManipulation("ibu pergi ke pasar bersama aku");
+
