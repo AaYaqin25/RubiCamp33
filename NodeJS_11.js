f9b24c5977b54246e96b982c9677f0f579fdb4ed
Ahmad Ainul Yaqin <bloodsakin@gmail.com>
2022-10-17 11:15:56 +0700

    challenge 11 done

---
+++ NodeJS_11.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const readline = require('node:readline');
+
+let data = fs.readFileSync('data.json', 'utf-8');
+let res = JSON.parse(data);
+
+console.log(`Selamat datang di permainan Tebak Kata, silahkan isi jawaban yang benar ya!`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Tebakan: '
+});
+let quest = 0;
+
+console.log(`Pertanyaan : ${res[quest].definition}`)
+
+rl.prompt();
+rl.on('line', (line) => {
+    if (quest < res.length - 1) {
+        if (line.trim().toLowerCase() == res[quest].term) {
+            quest++;
+            console.log("Selamat Anda Benar");
+            console.log(`Pertanyaan : ${res[quest].definition}`)
+            rl.prompt();
+        } else {
+            console.log("wkwkwk anda kurang beruntung");
+        }
+    } else if (line.trim() == res[quest].term) {
+        rl.close();
+        process.exit(0)
+    } else {
+        console.log("wkwkwk anda kurang beruntung");
+    }
+    rl.prompt();
+}).on('close', () => {
+    console.log('Hore Anda Menang!');
+});
