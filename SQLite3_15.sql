c80fbd8cbe17b3e8eea63095e2bb7eae763467d8
Ahmad Ainul Yaqin <bloodsakin@gmail.com>
2022-10-22 00:07:55 +0700

    challenge 15 done

---
+++ SQLite3_15.sql
@@ -0,0 +1,108 @@
+CREATE DATABASE univeruty.db
+
+CREATE TABLE mahasiswa (
+    nim varchar(50) primary key not null,
+    nama varchar(255) not null,
+    alamat text,
+    jurusan varchar(255) not null,
+    umur integer not null,
+    nilai varchar(255) not null,
+    SKS varchar(255) not null,
+    matkul varchar(255) not null
+);
+
+CREATE TABLE jurusan (
+    kodejurusan varchar(50) primary key not null,
+    namajurusan text not null
+);
+
+CREATE TABLE dosen (
+    iddosen varchar(50) primary key not null,
+    nama varchar(255) not null,
+    alamat varchar(255)
+);
+
+CREATE TABLE matakuliah (
+    kodematakuliah varchar(50) primary key not null,
+    nama varchar(255) not null,
+    sks varchar(255) not null
+);
+
+CREATE TABLE dosenmengajar(
+    id integer primary key autoincrement,
+    iddosen varchar(50) not null,
+    nim varchar(50) not null,
+    foreign key(iddosen) references dosen(iddosen),
+    foreign key(nim) references mahasiswa(nim)
+
+);
+
+CREATE TABLE nilaimahasiswa (
+    id integer primary key autoincrement,
+    kodematakuliah varchar(50) not null,
+    nim varchar(50) not null,
+    nilai integer not null,
+    foreign key(kodematakuliah) references matakuliah(kodematakuliah),
+    foreign key(nim) references mahasiswa(nim)
+
+);
+
+INSERT INTO mahasiswa VALUES 
+    ("A001", "Ahmad Ainul Yaqin", "Bogor", "Teknik Informatika", 25, "A", 11, "data mining"),
+    ("A002", "Tantowi Alif", "Cimahi", "Ekonomi", 24, "D", 12, "data mining"),
+    ("A003", "Bagas Kara", "Bandung", "Psikolog", 19, "B",13, "data mining"),
+    ("A004", "Muhammad Fajar", "Purwakarta", "Akuntansi", 18, "E", 10, "sastra");
+
+
+INSERT INTO jurusan VALUES 
+    ("H001", "Manajemen"),
+    ("H002", "Teknik Informatika"),
+    ("H003", "Akuntansi"),
+    ("H004", "Ekonomi");
+
+
+
+INSERT INTO dosen VALUES 
+    ("P001", "Rubi Henjayaa", "Bandung"),
+    ("P002", "Roberto Carlos", "Cimahi"),
+    ("P003", "Ronaldinho", "Bogor");
+
+
+INSERT INTO matakuliah VALUES 
+    ("Z001", "Matematika", 3),
+    ("Z002", "Agama", 2),
+    ("Z003", "Bahasa Inggris", 4);
+
+INSERT INTO dosenmengajar (iddosen, nim) VALUES
+    ("P001", "A001"),
+    ("P002", "A002"),
+    ("P003", "A003");
+
+INSERT INTO nilaimahasiswa (kodematakuliah, nim, nilai) VALUES
+    ("Z001", "A001", 80),
+    ("Z002", "A002", 70),
+    ("Z003", "A003", 85);
+
+-- 1
+SELECT * FROM mahasiswa;
+
+-- 2
+SELECT * FROM mahasiswa where umur < 20;
+
+-- 3 
+SELECT * FROM mahasiswa where nilai < "B";
+
+-- 4
+SELECT * FROM mahasiswa where SKS > 10;
+
+-- 5
+SELECT * FROM mahasiswa where matkul = "data mining";
+
+-- 6
+SELECT mahasiswa.nama as nama_mahasiswa, dosen.nama from dosenmengajar join mahasiswa on dosenmengajar.nim = mahasiswa.nim join dosen on dosenmengajar.iddosen = dosen.iddosen;
+
+-- 7
+SELECT * FROM mahasiswa ORDER BY umur;
+
+-- 8
+SELECT * FROM mahasiswa inner join dosen where nilai > "B";
