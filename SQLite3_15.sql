308ad92872e0dde5a1f797d1fd926313dc45bca3
Ahmad Ainul Yaqin <bloodsakin@gmail.com>
2022-10-22 17:47:31 +0700

    Revisi 15 SQL

--- SQLite3_15.sql
+++ SQLite3_15.sql
@@ -5,10 +5,7 @@ CREATE TABLE mahasiswa (
     nama varchar(255) not null,
     alamat text,
     jurusan varchar(255) not null,
-    umur integer not null,
-    nilai varchar(255) not null,
-    SKS varchar(255) not null,
-    matkul varchar(255) not null
+    umur integer not null
 );
 
 CREATE TABLE jurusan (
@@ -47,11 +44,21 @@ CREATE TABLE nilaimahasiswa (
 
 );
 
+CREATE TABLE jurusanMahaSiswa (
+    id integer primary key autoincrement,
+    kodejurusan varchar(50) not null,
+    nim varchar(50) not null,
+    foreign key(kodejurusan) references jurusan(kodejurusan),
+    foreign key(nim) references mahasiswa(nim)
+
+);
+
+
 INSERT INTO mahasiswa VALUES 
-    ("A001", "Ahmad Ainul Yaqin", "Bogor", "Teknik Informatika", 25, "A", 11, "data mining"),
-    ("A002", "Tantowi Alif", "Cimahi", "Ekonomi", 24, "D", 12, "data mining"),
-    ("A003", "Bagas Kara", "Bandung", "Psikolog", 19, "B",13, "data mining"),
-    ("A004", "Muhammad Fajar", "Purwakarta", "Akuntansi", 18, "E", 10, "sastra");
+    ("A001", "Ahmad Ainul Yaqin", "Bogor", "Teknik Informatika", 25),
+    ("A002", "Tantowi Alif", "Cimahi", "Ekonomi", 24),
+    ("A003", "Bagas Kara", "Bandung", "Psikolog", 19),
+    ("A004", "Muhammad Fajar", "Purwakarta", "Akuntansi", 18);
 
 
 INSERT INTO jurusan VALUES 
@@ -65,45 +72,60 @@ INSERT INTO jurusan VALUES
 INSERT INTO dosen VALUES 
     ("P001", "Rubi Henjayaa", "Bandung"),
     ("P002", "Roberto Carlos", "Cimahi"),
-    ("P003", "Ronaldinho", "Bogor");
+    ("P003", "Ronaldinho", "Bogor"),
+    ("P004", "Zidane", "Jakarta");
 
 
 INSERT INTO matakuliah VALUES 
-    ("Z001", "Matematika", 3),
-    ("Z002", "Agama", 2),
-    ("Z003", "Bahasa Inggris", 4);
+    ("Z001", "data mining", 11),
+    ("Z002", "data mining", 12),
+    ("Z003", "Bahasa Inggris", 13),
+    ("Z004", "Matematika", 9);
 
 INSERT INTO dosenmengajar (iddosen, nim) VALUES
     ("P001", "A001"),
     ("P002", "A002"),
-    ("P003", "A003");
+    ("P003", "A003"),
+    ("P004", "A004");
 
 INSERT INTO nilaimahasiswa (kodematakuliah, nim, nilai) VALUES
-    ("Z001", "A001", 80),
-    ("Z002", "A002", 70),
-    ("Z003", "A003", 85);
+    ("Z001", "A001", "A"),
+    ("Z002", "A002", "D"),
+    ("Z003", "A003", "E"),
+    ("Z004", "A004", "B");
+
+
+INSERT INTO jurusanMahaSiswa (kodejurusan, nim) VALUES
+    ("H001", "A001"),
+    ("H002", "A002"),
+    ("H003", "A003"),
+    ("H004", "A004");
+
 
 -- 1
-SELECT nim, nama, jurusan FROM mahasiswa;
+SELECT * FROM jurusanMahaSiswa join jurusan on jurusanMahaSiswa.kodejurusan = jurusan.kodejurusan join mahasiswa on jurusanMahaSiswa.nim = mahasiswa.nim;
 
--- 2
+-- 2 
 SELECT * FROM mahasiswa where umur < 20;
 
 -- 3 
-SELECT nama,nilai FROM mahasiswa where nilai < "B";
+SELECT * FROM nilaimahasiswa join matakuliah on nilaimahasiswa.kodematakuliah = matakuliah.kodematakuliah join mahasiswa on nilaimahasiswa.nim = mahasiswa.nim where nilaimahasiswa.nilai < "B";
 
 -- 4
-SELECT nama, SKS FROM mahasiswa where SKS > 10;
+SELECT * FROM nilaimahasiswa join matakuliah on nilaimahasiswa.kodematakuliah = matakuliah.kodematakuliah join mahasiswa on nilaimahasiswa.nim = mahasiswa.nim where matakuliah.sks like '1%';
+
 
 -- 5
-SELECT nama, matkul FROM mahasiswa where matkul = "data mining";
+SELECT * FROM nilaimahasiswa join matakuliah on nilaimahasiswa.kodematakuliah = matakuliah.kodematakuliah join mahasiswa on nilaimahasiswa.nim = mahasiswa.nim where matakuliah.nama = "data mining";
 
 -- 6
-SELECT COUNT(nama) FROM mahasiswa;  
+SELECT COUNT(*) FROM dosenmengajar join mahasiswa on dosenmengajar.nim = mahasiswa.nim join dosen on dosenmengajar.iddosen = dosen.iddosen;
 
 -- 7
 SELECT * FROM mahasiswa ORDER BY umur;
 
 -- 8
-SELECT nilai FROM mahasiswa where nilai > "B";
+SELECT * FROM nilaimahasiswa join matakuliah on nilaimahasiswa.kodematakuliah = matakuliah.kodematakuliah join mahasiswa on nilaimahasiswa.nim = mahasiswa.nim where nilaimahasiswa.nilai > "B";
+
 SELECT * FROM dosenmengajar join mahasiswa on dosenmengajar.nim = mahasiswa.nim join dosen on dosenmengajar.iddosen = dosen.iddosen;
+
